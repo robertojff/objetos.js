0e8b4fbc278c30d1faa7a2428d51d9438f293d83
robertojff <robertojff3333@gmail.com>
2023-12-16 20:04:23 -0300

    first commit

---
+++ objetos.js
@@ -0,0 +1,39 @@
+let pessoa = {
+    nome: 'Roberto',
+}
+
+console.log(pessoa);
+
+let segundaPessoa = {
+    nome:'Roberto',
+    idade: 21,
+    sobrenome:'De Jesus'
+}
+
+console.log(segundaPessoa);
+
+let terceiraPessoa = {
+    nome:'Ronaldo',
+    idade: 32,
+    sobrenome:'De Jesus'
+}
+
+console.log(terceiraPessoa);
+
+let quartaPessoa = {
+    nome:'Rodrigo',
+    idade: 35,
+    sobrenome:'De Jesus'
+}
+
+console.log(quartaPessoa);
+
+let quintaPessoa = {
+    nome:'Aguinaldo',
+    idade: 41,
+    sobrenome:'De Jesus'
+}
+
+console.log(quintaPessoa);
+
+
